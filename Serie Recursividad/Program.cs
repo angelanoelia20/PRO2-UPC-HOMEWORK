fee70c01fac5eb2b8db00affb2b864d6333fe2cb
= <=>
2023-11-23 17:31:35 -0400

    v5

---
+++ Serie Recursividad/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el número de términos para la serie de Fibonacci:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Serie de Fibonacci:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
